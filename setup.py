2bfda9c1ae17356d2edeec0b4d844a3fb60c2324
huto <t.hutzelmann@tum.de>
2016-05-20 17:33:01 +0200

    Check for minimal python version in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+import sys
+
+if sys.version_info[0] == 3 and sys.version_info[1] < 4:
+    sys.exit('Sorry, Python < 3.4 is not supported')
 
 setup(
     name='MakeLogic',
